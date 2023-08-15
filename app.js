4745a8b0f50ab3e9297d974718623d45fb21f56c
Megan Vyenielo <mvyenielo58@gmail.com>
2023-08-15 16:58:10 -0700

    completed submit form function

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 "use strict";
 
-async function submitForm() {
+async function submitForm(evt) {
+  evt.preventDefault();
   let searchTerm = $('#search-term').val();
 
-  const params = new URLSearchParams({searchTerm})
+  const params = new URLSearchParams({searchTerm});
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${params}
+    &api_key=J4hKUov2M8zKRFg7cMM7DkOAQUHnjDBi`);
+  const data = await response.json();
+  console.log(`data is`, data);
+  console.log(searchTerm);
+  addToGallery(data);
 }
 
-$("#form").on('submit', '#input', submitForm);
+function addToGallery(gifs) {
+  let randomIndex = Math.floor(Math.random() * gifs.data.length);
+  let chosenGif = gifs.data[randomIndex];
+
+  $(".gallery").append($(`<img src=${chosenGif}>`));
+}
+
+$("#submit").on('click', submitForm);
