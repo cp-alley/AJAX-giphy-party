c5f5ae3da54f3b03d6f5a0470002110d27426355
Christopher Alley <calleyviola@gmail.com>
2023-08-15 20:20:14 -0400

    Add delete function

--- app.js
+++ app.js
@@ -4,20 +4,30 @@ async function submitForm(evt) {
   evt.preventDefault();
   let searchTerm = $('#search-term').val();
 
-  const params = new URLSearchParams({searchTerm});
+  const params = new URLSearchParams({ searchTerm });
   const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${params}
     &api_key=J4hKUov2M8zKRFg7cMM7DkOAQUHnjDBi`);
   const data = await response.json();
+
   console.log(`data is`, data);
   console.log(searchTerm);
+
   addToGallery(data);
 }
 
 function addToGallery(gifs) {
   let randomIndex = Math.floor(Math.random() * gifs.data.length);
-  let chosenGif = gifs.data[randomIndex];
+  let chosenGif = gifs.data[randomIndex].images.original.url;
 
-  $(".gallery").append($(`<img src=${chosenGif}>`));
+  console.log("chosen gif=", chosenGif);
+
+  $(".gallery").append($(`<img src="${chosenGif}">`));
+}
+
+function deleteImages() {
+  $(".gallery").empty();
 }
 
 $("#submit").on('click', submitForm);
+
+$("#delete").on('click', deleteImages);
