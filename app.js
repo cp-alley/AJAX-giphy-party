59a1a2abe5b426e4b61ac88fdd36dfca3f26ce2d
Christopher Alley <calleyviola@gmail.com>
2023-08-15 20:37:10 -0400

    Code review suggestions

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 "use strict";
 
+//TODO: make doc strings
+
+const GIPHY_API_KEY = 'J4hKUov2M8zKRFg7cMM7DkOAQUHnjDBi';
+const GIPHY_API_URL = 'http://api.giphy.com/v1/gifs/search';
+
+//TODO: better function name, params utilize global constants
 async function submitForm(evt) {
   evt.preventDefault();
-  let searchTerm = $('#search-term').val();
+  const searchTerm = $('#search-term').val();
 
-  const params = new URLSearchParams({ searchTerm });
+  const params = new URLSearchParams({ searchTerm, api_key : GIPHY_API_KEY });
   const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${params}
     &api_key=J4hKUov2M8zKRFg7cMM7DkOAQUHnjDBi`);
   const data = await response.json();
@@ -15,6 +21,7 @@ async function submitForm(evt) {
   addToGallery(data);
 }
 
+//TODO: create separate random function
 function addToGallery(gifs) {
   let randomIndex = Math.floor(Math.random() * gifs.data.length);
   let chosenGif = gifs.data[randomIndex].images.original.url;
